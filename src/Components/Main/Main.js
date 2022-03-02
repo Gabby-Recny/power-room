235d38be15d36d1aed512a98854c2994f2b1dcd2
Gabby Recny <gjtrecny@gmail.com>
2022-03-02 11:03:01 -0600

    Install SASS and main page

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Main.scss';
+
+const Main = () => {
+    return (
+        <section className='main-page'>
+            <h2>Main</h2>
+        </section>
+    )
+}
+
+export default Main;

6ec1f67fc997c84097e819cd2057119cda6fb07e
Gabby Recny <gjtrecny@gmail.com>
2022-03-02 16:49:31 -0600

    Add router

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -2,8 +2,6 @@ import { cleanBathroomAPI } from "./dataCleaning";
 import { cleanZipCodeAPI } from "./dataCleaning";
 
 export const getBathrooms = (lat, long) => {
-    //If this fetch call doesn't work with ZIP code API, look at the minus symbol "-" before longitude;
-
     return fetch(`https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=100&offset=0&lat=${lat}&lng=${long}`)
         .then(response => checkResponse(response))
         .then(data => cleanBathroomAPI(data))
@@ -20,7 +18,7 @@ export const getZipCode = (zipcode) => {
 
 const checkResponse = (response) => {
     if (!response.ok) {
-        console.log('Response is not okay', response)
+        console.log('!Response.ok', response)
         throw new Error (`ERROR`)
     } else {
         return response.json()

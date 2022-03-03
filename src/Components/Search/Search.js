921f810998eb32f4bed4f2e2d1fad6a7816c0da0
Gabby Recny <gjtrecny@gmail.com>
2022-03-02 18:02:52 -0600

    Import icon files

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import './Search.scss';
 import { getBathrooms, getZipCode } from "../../utilities/apiCalls";
+import ResultsContainer from "../Results/ResultsContainer/ResultsContainer";
 
 const Search = () => {
 
 const [ postalCode, setPostalCode ] = useState([]);
+const [ results, setResults ] = useState([]);
 
 const handleClick = (event) => {
     event.preventDefault()
     getZipCode(postalCode)
     .then(location => getBathrooms(location.latitude, location.longitude))
-
+    .then(bathrooms => setResults(bathrooms))
+    .catch(err => console.log(err))
 }
-
     return (
         <section className='search-section'>
             <form className='search-bar'>
@@ -29,6 +31,7 @@ const handleClick = (event) => {
                 onClick={event => handleClick(event)}
                 >Search Now</button>
             </form>
+            <ResultsContainer results={results}/>
         </section>
     )
 }

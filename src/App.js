420e0e6dcff31587069ea4313d4f788ba08f5841
Gabby Recny <gjtrecny@gmail.com>
2022-03-04 15:23:16 -0600

    Add routing to bathroom details and error handling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
-import Search from './Components/Search/Search';
 import Main from './Components/Search/Main'
 import Header from './Components/Header/Header';
-import ResultsContainer from './Components/Results/ResultsContainer/ResultsContainer';
 import Error from './Components/Error/Error';
 import BathroomDetails from './Components/BathroomDetails/BathroomDetails';
-import Results from './Components/Results/ResultsContainer/Results';
 
 const App = () => {
 
@@ -17,17 +14,14 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path="/" component={Main} />
-        {/* <Route exact path="/" component={Results} /> */}
-        {/* <Route exact path='/' component={Results} /> */}
-        {/* <Route path='/:postalCode' 
+        <Route path='/:id'
           render={( {match} ) => {
-            return <ResultsContainer postalCode={match.params.postalCode} />
-          }} />
-        <Route exact path="/error" component={Error}></Route> */}
-        {/* <Route path='/:postalCode/:id'
-          render={( {match} ) => {
-            return <BathroomDetails postalCode={} id={} />
-          }} */}
+            return <BathroomDetails id={match.params.id} />
+            }
+          }>
+        </Route>
+        <Route exact path='/error' component={Error}/>
+        <Redirect to='/error' />
       </Switch>
     </section>
   )
--- src/Components/Results/ResultsContainer/Results.js
+++ src/Components/Results/ResultsContainer/Results.js
@@ -1,31 +1,34 @@
 import React from "react";
 import ResultCard from "../ResultsCard/ResultCard";
+import { Link } from "react-router-dom";
 
 const Results = ({ results }) => {
-    console.log('Results Line 5', results)
     const bathroomResults = results.map(result => {
         return (
-            <ResultCard 
-            key={result.id}
-            id={result.id}
-            name={result.name}
-            street= {result.street}
-            city={result.city}
-            state={result.state}
-            accessible={result.accessible}
-            unisex={result.unisex}
-            directions={result.directions}
-            comment={result.comment}
-            latitude={result.latitude}
-            longitude={result.longitude}
-            downvote={result.downvote}
-            upvote={result.upvote}
-            country={result.country}
-            changing_table={result.changing_table}
-            approved={result.approved}
-            distance={result.distance}
-    
-        />
+            <Link 
+                to={`/${result.id}`} 
+                key={result.id}>
+                <ResultCard 
+                // key={result.id}
+                    id={result.id}
+                    name={result.name}
+                    street= {result.street}
+                    city={result.city}
+                    state={result.state}
+                    accessible={result.accessible}
+                    unisex={result.unisex}
+                    directions={result.directions}
+                    comment={result.comment}
+                    latitude={result.latitude}
+                    longitude={result.longitude}
+                    downvote={result.downvote}
+                    upvote={result.upvote}
+                    country={result.country}
+                    changing_table={result.changing_table}
+                    approved={result.approved}
+                    distance={result.distance}
+                />
+            </Link>
         )
         })
     

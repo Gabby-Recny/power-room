570ca25f8d6e6919fcc19573904cfab401268a24
Gabby Recny <gjtrecny@gmail.com>
2022-03-05 00:43:04 -0600

    Style result card layout

--- src/Components/Results/ResultsCard/ResultCard.js
+++ src/Components/Results/ResultsCard/ResultCard.js
@@ -7,19 +7,28 @@ import lgbt from '../../../Assets/lgbt.png';
 const ResultCard = (props) => {
     return (
             <article className='result-card'>
-                <h2>{props.name}</h2>
-                <h5>{props.street}</h5>
-                <h5>{props.city}</h5>
-                <h5>{props.state}</h5>
-                <section className='restroom-icons'>
-                    {props.accessible && <img src={ADAIcon} alt='Verified accessible restroom' />}
-                    {props.unisex && <img src={lgbt} alt='Verified gender neutral and/or unisex restroom' />}
-                    {props.changing_table && <img src={babyBoy} alt='Verified changing table in the restroom' />}
+                <section className='primary-info'>
+                    <section className='address-info'>
+                        <h2>{props.name}</h2>
+                        <h5>{props.street}</h5>
+                        <h5>{props.city}, {props.state}</h5>
+                    </section>
+                    <section className='restroom-icons'>
+                        {props.accessible && <img src={ADAIcon} alt='Verified accessible restroom' />}
+                        {props.unisex && <img src={lgbt} alt='Verified gender neutral and/or unisex restroom' />}
+                        {props.changing_table && <img src={babyBoy} alt='Verified changing table in the restroom' />}
+                    </section>
                 </section>
-                <h5>Upvotes: {props.upvote}</h5>
-                <h5>Downvotes: {props.downvote}</h5>
-                <h5>{props.approved && <h5>Approved</h5>}</h5>
-                <h5>{props.directions}</h5>
+                <section className='ratings'>
+                    <div>
+                        <h5>Upvotes: {props.upvote}</h5>
+                        <h5>Downvotes: {props.downvote}</h5>
+                    </div>
+                    {props.approved && <h5>Approved</h5>}
+                </section>
+                <section className='directions'>
+                    <h5>Directions: {props.directions}</h5>
+                </section>
             </article>
     )
 }

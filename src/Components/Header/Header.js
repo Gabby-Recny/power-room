2e67c96e09f52998c283e79ff0ab5224efe9b3fe
Gabby Recny <gjtrecny@gmail.com>
2022-03-04 23:04:25 -0600

    Implement hamburger button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,43 @@
-import React from "react";
-import './Header.scss'
+import React, { useState } from "react";
+import './Header.scss';
+import Menu from '../Menu/Menu';
+import menuIcon from '../../Assets/menu.png'
 
 const Header = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const toggleClick = () => {
+        setClicked(!clicked)
+    };
     return (
-        <header>
+        <>
+        <nav>
+            <section>
+                <button className='burger-button' data-testid='burger-button' onClick={toggleClick} type="button">
+                    <img className='menu-icon' alt='hamburger menu button' src={menuIcon} />
+                </button>
             <h1>Power Room</h1>
-        </header>
+            </section>
+            <div className="menu" data-testid='menu'>{clicked && <Menu />}</div>
+        </nav>
+
+    </>
     )
 }
 
 export default Header;
+
+
+
+
+// import React, { useContext, useState } from "react";
+// import { NavLink } from "react-router-dom";
+// import { UserContext } from "../Contexts/UserContext";
+// import Menu from "./HamburgerMenu";
+// import menuIcon from "../Assets/menuIcon.png"
+// import './Nav.scss'
+
+
+
+
+
